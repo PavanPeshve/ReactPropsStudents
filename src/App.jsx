e4eeee65aba29067cc8cb74ce4f01e6e7f440d0e
PavanPeshve <pavanpeshve99@gmail.com>
2023-03-15 23:45:16 +0530

    counterpush

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
-import studentDetails from './studentdetail.json'
-import Allstudents from './Allstudents.jsx';
+import './general.css'
+
 export default class App extends Component {
   constructor(){
     super()
     this.state={
-      json:studentDetails
+      counter:0
     }
+  }
+  countAppreciate=()=>{
+    this.setState({count: this.state.counter++})
+  }
+  countDepreciate=()=>{
+    this.setState({count: this.state.counter--})
+  }
+  countReset=()=>{
+    this.setState({counter: 0})
   }
   render() {
     return (
-      <>
-      <Allstudents data={this.state.json}/>
-      </>
+      <div className='mainclass'>
+      <h1>{this.state.counter}</h1>
+      <div className='buttons'>
+        <button onClick={this.countAppreciate} id="btn1">APPRECIATE</button>
+        <button onClick={this.countDepreciate} id="btn1">DEPRECIATE</button>
+        <button onClick={this.countReset} id="btn1">RESET</button>
+      </div>
+      </div>
     )
   }
 }
